67f573a9bbc0c497b9013adbcab2e65ea43c6bd7
LinZ-debug <zhaochy1@shanghaitech.edu.cn>
2020-12-12 18:43:21 +0800

    `humanAgent` finished!

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
-from util import *
 import time
 import os
 import traceback
 import sys
+import inspect
 
 class Agent:
     """
@@ -16,6 +16,9 @@ class Agent:
         self.cards = cards
         self.isLord = isLord
 
+    def getName(self):
+        return self.name
+
     def isWinner(self):
         return True if len(self.cards)==0 else False
 
@@ -26,12 +29,56 @@ class Agent:
         """
         raiseNotDefined()
 
-class HumanAgent(Agent):
-    def gatAction(self, state):
-        choice = input("player %s: please enter your choics:"%(self.name))
+    # update leagal cards
+    def updateCards(self, cards_out):
+        for card_out in cards_out:
+            for card in self.cards:
+                if card[2:] == str(card_out):
+                    self.cards.remove(card)
 
-        
-        return choice
+    def takeAction(self, state):
+        action = self.getAction(state)
+        if action == None:
+            return self.cards
+        else:
+            self.updateCards(action)
+            return self.cards
+
+
+class HumanAgent(Agent):
+    def getAction(self, state):
+        choice = input("player %s: \nplease enter your card choices in ONE LINE:"%(self.name))
+        cards_out = choice.split()
+        # if pass
+        if cards_out == []:
+            return None
+
+        # check if input valid
+        # if not recurse
+        print("Your choice: ", cards_out)
+        for card_out in cards_out:
+            find = False
+            for card in self.cards:
+                if card[1:] == str(card_out) or card == card_out:
+                    self.cards.remove(card)
+                    find = True
+            if not find:
+                print("invalid card choice!! Enter again!!\n") 
+                return self.getAction(state)
+
+        return cards_out
+
+
+######################## helper function ##############################
+
+def raiseNotDefined():
+    fileName = inspect.stack()[1][1]
+    line = inspect.stack()[1][2]
+    method = inspect.stack()[1][3]
+
+    print("*** Method not implemented: %s at line %s of %s" %
+          (method, line, fileName))
+    sys.exit(1)
 
 
 
--- play.py
+++ play.py
@@ -1,4 +1,19 @@
 from util import *
+from game import *
 
+
+name = input("enter your name:")
+# list = input("player %s: \nplease enter your card choices in ONE LINE:"%(name))
+# input()
+# print("list:", list)
+# print("list:", list.split())
+# print("is empty:", list.split())
+# print("list:", newlist) 
+print("delivering")
 deliver()
-print_cards()
+player = HumanAgent(name, w1, True)
+
+while not player.isWinner():
+    print("your cards:", player.cards)
+    player.takeAction(None)
+print("finish!")
